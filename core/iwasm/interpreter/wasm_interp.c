26eb5f786cc34a6a92fa0881421ab852ec4f22bc
Huang, Wenyong <wenyong.huang@intel.com>
2020-02-07 18:17:29 +0800

    Refine read leb in interpreter
    Change-Id: I7d1b2cfb03856fe4a2c1a8e7640382fb6fb6cfdc

--- core/iwasm/interpreter/wasm_interp.c
+++ core/iwasm/interpreter/wasm_interp.c
@@ -525,18 +525,42 @@ read_leb(const uint8 *buf, uint32 *p_offset, uint32 maxbits, bool sign)
   } while (0)
 
 #define read_leb_int64(p, p_end, res) do {      \
+  uint8 _val = *p;                              \
+  if (!(_val & 0x80)) {                         \
+    res = (int64)_val;                          \
+    if (_val & 0x40)                            \
+      /* sign extend */                         \
+      res |= 0xFFFFFFFFFFFFFF80LL;              \
+    p++;                                        \
+    break;                                      \
+  }                                             \
   uint32 _off = 0;                              \
   res = (int64)read_leb(p, &_off, 64, true);    \
   p += _off;                                    \
 } while (0)
 
 #define read_leb_uint32(p, p_end, res) do {     \
+  uint8 _val = *p;                              \
+  if (!(_val & 0x80)) {                         \
+    res = _val;                                 \
+    p++;                                        \
+    break;                                      \
+  }                                             \
   uint32 _off = 0;                              \
   res = (uint32)read_leb(p, &_off, 32, false);  \
   p += _off;                                    \
 } while (0)
 
 #define read_leb_int32(p, p_end, res) do {      \
+  uint8 _val = *p;                              \
+  if (!(_val & 0x80)) {                         \
+    res = (int32)_val;                          \
+    if (_val & 0x40)                            \
+      /* sign extend */                         \
+      res |= 0xFFFFFF80;                        \
+    p++;                                        \
+    break;                                      \
+  }                                             \
   uint32 _off = 0;                              \
   res = (int32)read_leb(p, &_off, 32, true);    \
   p += _off;                                    \
