b5c28278076ca5efa2a44af8d420d9420411e88f
Xu, Jun1 <jun1.xu@intel.com>
2020-02-13 09:31:05 +0800

    add copyright for assemblyscript library
    Change-Id: If6e211d880650397e6c5494c15a975e27ad94315

--- assembly-script/wamr_app_lib/console.ts
+++ assembly-script/wamr_app_lib/console.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 @external("env", "printf")
 declare function printf(a: ArrayBuffer): i32;
 
--- assembly-script/wamr_app_lib/timer.ts
+++ assembly-script/wamr_app_lib/timer.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 @external("env", "wasm_create_timer")
 declare function wasm_create_timer(a: i32, b: bool, c: bool): i32;
 
