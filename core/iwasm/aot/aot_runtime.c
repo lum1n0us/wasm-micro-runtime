44aeecd7b9549871ae781dfeccd28cfa91cc5e12
Huang, Wenyong <wenyong.huang@intel.com>
2020-04-07 05:01:57 +0800

    Fix function type not set issue of aot_call_indirect
    Change-Id: Ie47d73f1c4b8d3c81efe96623871f10a0c183dc6

--- core/iwasm/aot/aot_runtime.c
+++ core/iwasm/aot/aot_runtime.c
@@ -864,8 +864,7 @@ aot_call_indirect(WASMExecEnv *exec_env,
     AOTModule *aot_module = (AOTModule*)module_inst->aot_module.ptr;
     uint32 *func_type_indexes = (uint32*)module_inst->func_type_indexes.ptr;
     uint32 *table_data = (uint32*)module_inst->table_data.ptr;
-    AOTFuncType *func_type = check_func_type ?
-            aot_module->func_types[func_type_idx] : NULL;
+    AOTFuncType *func_type;
     void **func_ptrs = (void**)module_inst->func_ptrs.ptr, *func_ptr;
     uint32 table_size = module_inst->table_size;
     uint32 func_idx, func_type_idx1;
@@ -886,13 +885,14 @@ aot_call_indirect(WASMExecEnv *exec_env,
     }
 
     func_type_idx1 = func_type_indexes[func_idx];
-    if (check_func_type &&
-            !aot_is_wasm_type_equal(module_inst, func_type_idx,
-                                    func_type_idx1)) {
+    if (check_func_type
+        && !aot_is_wasm_type_equal(module_inst, func_type_idx,
+                                   func_type_idx1)) {
         aot_set_exception_with_id(module_inst,
                                   EXCE_INVALID_FUNCTION_TYPE_INDEX);
         return false;
     }
+    func_type = aot_module->func_types[func_type_idx1];
 
     if (!(func_ptr = func_ptrs[func_idx])) {
         bh_assert(func_idx < aot_module->import_func_count);
