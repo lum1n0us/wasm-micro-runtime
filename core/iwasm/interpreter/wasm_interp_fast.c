39498cb0ec9f33f78759bcf850d222f55800a77b
Xu, Jun1 <jun1.xu@intel.com>
2020-04-09 14:25:13 +0800

    fix problem of float in x86_32
    Change-Id: Ifba2fd4b5a24b1fc58eafd2b39b2eea0993a1bb6

--- core/iwasm/interpreter/wasm_interp_fast.c
+++ core/iwasm/interpreter/wasm_interp_fast.c
@@ -502,8 +502,9 @@ read_leb(const uint8 *buf, uint32 *p_offset, uint32 maxbits, bool sign)
   } while (0)
 
 #define DEF_OP_REINTERPRET(src_type) do {                                   \
-    SET_OPERAND(src_type, 2, GET_OPERAND(src_type, 0));                     \
-    frame_ip += 4;                                                          \
+    void *src = frame_lp + GET_OFFSET();                                    \
+    void *dst = frame_lp + GET_OFFSET();                                    \
+    bh_memcpy_s(dst, sizeof(src_type), src, sizeof(src_type));              \
   } while (0)
 
 #if WASM_CPU_SUPPORTS_UNALIGNED_64BIT_ACCESS != 0
@@ -929,10 +930,16 @@ wasm_interp_call_func_bytecode(WASMModuleInstance *module,
           addr2 = GET_OFFSET();
           addr_ret = GET_OFFSET();
 
-          if (!cond)
-            frame_lp[addr_ret] = frame_lp[addr1];
-          else
-            frame_lp[addr_ret] = frame_lp[addr2];
+          if (!cond) {
+            if (addr_ret != addr1)
+              bh_memcpy_s(frame_lp + addr_ret, sizeof(int32),
+                          frame_lp + addr1, sizeof(int32));
+          }
+          else {
+            if (addr_ret != addr2)
+              bh_memcpy_s(frame_lp + addr_ret, sizeof(int32),
+                          frame_lp + addr2, sizeof(int32));
+          }
           HANDLE_OP_END ();
         }
 
@@ -943,10 +950,16 @@ wasm_interp_call_func_bytecode(WASMModuleInstance *module,
           addr2 = GET_OFFSET();
           addr_ret = GET_OFFSET();
 
-          if (!cond)
-            *(int64*)(frame_lp + addr_ret) = *(int64*)(frame_lp + addr1);
-          else
-            *(int64*)(frame_lp + addr_ret) = *(int64*)(frame_lp + addr2);
+          if (!cond) {
+            if (addr_ret != addr1)
+              bh_memcpy_s(frame_lp + addr_ret, sizeof(int64),
+                          frame_lp + addr1, sizeof(int64));
+          }
+          else {
+            if (addr_ret != addr2)
+              bh_memcpy_s(frame_lp + addr_ret, sizeof(int64),
+                          frame_lp + addr2, sizeof(int64));
+          }
           HANDLE_OP_END ();
         }
 
@@ -2061,13 +2074,15 @@ wasm_interp_call_func_bytecode(WASMModuleInstance *module,
       HANDLE_OP (EXT_OP_COPY_STACK_TOP):
         addr1 = GET_OFFSET();
         addr2 = GET_OFFSET();
-        frame_lp[addr2] = frame_lp[addr1];
+        bh_memcpy_s(frame_lp + addr2, sizeof(int32),
+                    frame_lp + addr1, sizeof(int32));
         HANDLE_OP_END ();
 
       HANDLE_OP (EXT_OP_COPY_STACK_TOP_I64):
         addr1 = GET_OFFSET();
         addr2 = GET_OFFSET();
-        *(float64*)(frame_lp + addr2) = *(float64*)(frame_lp + addr1);
+        bh_memcpy_s(frame_lp + addr2, sizeof(int64),
+                    frame_lp + addr1, sizeof(int64));
         HANDLE_OP_END ();
 
       HANDLE_OP (WASM_OP_SET_LOCAL):
