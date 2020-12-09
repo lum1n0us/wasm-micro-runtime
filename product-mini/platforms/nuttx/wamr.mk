bf523554be8ccc3e42a3efe61882bc57e1108b1d
Huang, Wenyong <wenyong.huang@intel.com>
2020-12-09 10:08:12 +0800

    Sync up with github: update NuttX platform implementation
    Change-Id: Iffc828789ec7a46ab0cebf064bb33f18e29c1a60

--- product-mini/platforms/nuttx/wamr.mk
+++ product-mini/platforms/nuttx/wamr.mk
@@ -107,6 +107,13 @@ else
 CFLAGS += -DWASM_ENABLE_THREAD_MGR=0
 endif
 
+ifeq ($(CONFIG_INTERPRETERS_WAMR_LIB_PTHREAD),y)
+CFLAGS += -DWASM_ENABLE_LIB_PTHREAD=1
+CSRCS += lib_pthread_wrapper.c
+else
+CFLAGS += -DWASM_ENABLE_LIB_PTHREAD=0
+endif
+
 ifeq ($(CONFIG_INTERPRETERS_WAMR_MINILOADER),y)
 CFLAGS += -DWASM_ENABLE_MINI_LOADER=1
 CSRCS += wasm_mini_loader.c
@@ -140,6 +147,7 @@ CFLAGS += -Wno-int-conversion -Wno-implicit-function-declaration
 CFLAGS += -I${CORE_ROOT} \
 		      -I${IWASM_ROOT}/include \
           -I${IWASM_ROOT}/common \
+          -I${IWASM_ROOT}/libraries/thread-mgr \
           -I${SHARED_ROOT}/include \
           -I${SHARED_ROOT}/platform/include \
           -I${SHARED_ROOT}/utils \
@@ -153,7 +161,8 @@ CFLAGS += -I${IWASM_ROOT}/interpreter
 endif
 
 CSRCS += nuttx_platform.c \
-         nuttx_thread.c \
+         posix_thread.c \
+         posix_time.c \
          mem_alloc.c \
          ems_kfc.c \
          ems_alloc.c \
@@ -176,6 +185,7 @@ CSRCS += nuttx_platform.c \
 ASRCS += ${INVOKE_NATIVE}
 
 VPATH += ${SHARED_ROOT}/platform/nuttx
+VPATH += ${SHARED_ROOT}/platform/common/posix
 VPATH += ${SHARED_ROOT}/mem-alloc
 VPATH += ${SHARED_ROOT}/mem-alloc/ems
 VPATH += ${SHARED_ROOT}/utils
@@ -184,6 +194,7 @@ VPATH += ${IWASM_ROOT}/common
 VPATH += ${IWASM_ROOT}/interpreter
 VPATH += ${IWASM_ROOT}/libraries
 VPATH += ${IWASM_ROOT}/libraries/libc-builtin
+VPATH += ${IWASM_ROOT}/libraries/lib-pthread
 VPATH += ${IWASM_ROOT}/common/arch
 VPATH += ${IWASM_ROOT}/aot
 VPATH += ${IWASM_ROOT}/aot/arch
