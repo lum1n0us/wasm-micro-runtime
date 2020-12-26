76a506211ac417f75cb30b811fd82800fef8e22b
Huang, Wenyong <wenyong.huang@intel.com>
2020-12-27 03:11:08 +0800

    Refine classic interpreter to reduce count of find_block_addr
    For test case tsf, switch2, gcc-loops, there is switch..case sentence
    with lots of case branches, these introduce many block opcodes before
    br_table opcode, which causes many duplicated find_block_addr callings
    and makes cases run very slow.
    
    Change-Id: I002c18a6b755ba972c23b91897859b06c9f633e6

--- core/iwasm/interpreter/wasm_interp_classic.c
+++ core/iwasm/interpreter/wasm_interp_classic.c
@@ -423,8 +423,9 @@ read_leb(const uint8 *buf, uint32 *p_offset, uint32 maxbits, bool sign)
 
 #define PUSH_CSP(_label_type, cell_num, _target_addr) do {   \
     bh_assert(frame_csp < frame->csp_boundary);              \
-    frame_csp->label_type = _label_type;                     \
+    /* frame_csp->label_type = _label_type; */               \
     frame_csp->cell_num = cell_num;                          \
+    frame_csp->begin_addr = frame_ip;                        \
     frame_csp->target_addr = _target_addr;                   \
     frame_csp->frame_sp = frame_sp;                          \
     frame_csp++;                                             \
@@ -1125,15 +1126,9 @@ handle_op_block:
         else if (cache_items[1].start_addr == frame_ip) {
           end_addr = cache_items[1].end_addr;
         }
-        else if (!wasm_loader_find_block_addr((BlockAddr*)exec_env->block_addr_cache,
-                                              frame_ip, (uint8*)-1,
-                                              LABEL_TYPE_BLOCK,
-                                              &else_addr, &end_addr,
-                                              NULL, 0)) {
-          wasm_set_exception(module, "find block address failed");
-          goto got_exception;
+        else {
+          end_addr = NULL;
         }
-
         PUSH_CSP(LABEL_TYPE_BLOCK, cell_num, end_addr);
         HANDLE_OP_END ();
 
@@ -1171,8 +1166,7 @@ handle_op_if:
         else if (!wasm_loader_find_block_addr((BlockAddr*)exec_env->block_addr_cache,
                                               frame_ip, (uint8*)-1,
                                               LABEL_TYPE_IF,
-                                              &else_addr, &end_addr,
-                                              NULL, 0)) {
+                                              &else_addr, &end_addr)) {
           wasm_set_exception(module, "find block address failed");
           goto got_exception;
         }
@@ -1220,6 +1214,16 @@ handle_op_if:
         read_leb_uint32(frame_ip, frame_ip_end, depth);
 label_pop_csp_n:
         POP_CSP_N(depth);
+        if (!frame_ip) { /* must be label pushed by WASM_OP_BLOCK */
+          if (!wasm_loader_find_block_addr((BlockAddr*)exec_env->block_addr_cache,
+                                           (frame_csp - 1)->begin_addr, (uint8*)-1,
+                                           LABEL_TYPE_BLOCK,
+                                           &else_addr, &end_addr)) {
+            wasm_set_exception(module, "find block address failed");
+            goto got_exception;
+          }
+          frame_ip = end_addr;
+        }
         HANDLE_OP_END ();
 
       HANDLE_OP (WASM_OP_BR_IF):
