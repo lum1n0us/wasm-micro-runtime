2ebfd264108f13726502b718de34bc73ed1fbd84
Huang, Wenyong <wenyong.huang@intel.com>
2021-01-17 23:59:47 +0800

    Fix wamrc --help wrong line order issue
    Change-Id: I9ab4a9fbd9d593caea005ca534a5e49f6ac7b823

--- wamr-compiler/main.c
+++ wamr-compiler/main.c
@@ -41,8 +41,8 @@ print_help()
   printf("                              llvmir-opt     Optimized LLVM IR\n");
   printf("  --enable-bulk-memory      Enable the post-MVP bulk memory feature\n");
   printf("  --enable-multi-thread     Enable multi-thread feature, the dependent features bulk-memory and\n");
-  printf("  --enable-tail-call        Enable the post-MVP tail call feature\n");
   printf("                            thread-mgr will be enabled automatically\n");
+  printf("  --enable-tail-call        Enable the post-MVP tail call feature\n");
   printf("  --enable-simd             Enable the post-MVP 128-bit SIMD feature\n");
   printf("  --enable-dump-call-stack  Enable stack trace feature\n");
   printf("  -v=n                      Set log verbose level (0 to 5, default is 2), larger with more log\n");
