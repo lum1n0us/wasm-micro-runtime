b95dabb50d5d90d10a13c7534a532fb20618760b
Huang, Wenyong <wenyong.huang@intel.com>
2021-03-03 09:55:45 +0800

    Remove payload size no larger than 1M limit, re-org codes
    Change-Id: Ibd86f44b11b376d71175114a3c7787b3d694e9c6

--- core/app-mgr/app-manager/app_manager_host.c
+++ core/app-mgr/app-manager/app_manager_host.c
@@ -13,11 +13,18 @@
 static host_interface host_commu;
 
 /* IMRTLink Two leading bytes */
-static unsigned char leadings[] = { (unsigned char) 0x12, (unsigned char) 0x34 };
+static unsigned char leadings[] = {
+    (unsigned char)0x12,
+    (unsigned char)0x34
+};
 
 /* IMRTLink Receiving Phase */
 typedef enum recv_phase_t {
-    Phase_Non_Start, Phase_Leading, Phase_Type, Phase_Size, Phase_Payload
+    Phase_Non_Start,
+    Phase_Leading,
+    Phase_Type,
+    Phase_Size,
+    Phase_Payload
 } recv_phase_t;
 
 /* IMRTLink Receive Context */
@@ -74,7 +81,8 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
         }
 
         return 0;
-    } else if (ctx->phase == Phase_Leading) {
+    }
+    else if (ctx->phase == Phase_Leading) {
         if (ch == leadings[1]) {
             if (enable_log)
                 app_manager_printf("##On byte arrive: got leading 1\n");
@@ -83,12 +91,14 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
             ctx->phase = Phase_Non_Start;
 
         return 0;
-    } else if (ctx->phase == Phase_Type) {
+    }
+    else if (ctx->phase == Phase_Type) {
         if (ctx->size_in_phase++ == 0) {
             if (enable_log)
                 app_manager_printf("##On byte arrive: got type 0\n");
             ctx->message.message_type = ch;
-        } else {
+        }
+        else {
             if (enable_log)
                 app_manager_printf("##On byte arrive: got type 1\n");
             ctx->message.message_type |= (ch << 8);
@@ -98,7 +108,8 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
         }
 
         return 0;
-    } else if (ctx->phase == Phase_Size) {
+    }
+    else if (ctx->phase == Phase_Size) {
         unsigned char *p = (unsigned char *) &ctx->message.payload_size;
 
         if (enable_log)
@@ -122,14 +133,9 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
             if (ctx->message.payload_size == 0) {
                 ctx->phase = Phase_Non_Start;
                 if (enable_log)
-                    app_manager_printf(
-                            "##On byte arrive: receive end, payload_size is 0.\n");
+                    app_manager_printf("##On byte arrive: receive end, "
+                                       "payload_size is 0.\n");
                 return 1;
-            }
-
-            if (ctx->message.payload_size > 1024 * 1024) {
-                ctx->phase = Phase_Non_Start;
-                return 0;
             }
 
             if (ctx->message.message_type != INSTALL_WASM_APP) {
@@ -146,7 +152,8 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
         }
 
         return 0;
-    } else if (ctx->phase == Phase_Payload) {
+    }
+    else if (ctx->phase == Phase_Payload) {
         if (ctx->message.message_type == INSTALL_WASM_APP) {
             int received_size;
             module_on_install_request_byte_arrive_func module_on_install =
@@ -162,25 +169,29 @@ static int on_imrt_link_byte_arrive(unsigned char ch, recv_context_t *ctx)
                         ctx->phase = Phase_Non_Start;
                         return 1;
                     }
-                } else {
+                }
+                else {
                     /* receive or handle fail */
                     ctx->phase = Phase_Non_Start;
                     ctx->size_in_phase = 0;
                     return 0;
                 }
                 return 0;
-            } else {
+            }
+            else {
                 ctx->phase = Phase_Non_Start;
                 ctx->size_in_phase = 0;
                 return 0;
             }
-        } else {
+        }
+        else {
             ctx->message.payload[ctx->size_in_phase++] = ch;
 
             if (ctx->size_in_phase == ctx->message.payload_size) {
                 ctx->phase = Phase_Non_Start;
                 if (enable_log)
-                    app_manager_printf("##On byte arrive: receive end, payload_size is %d.\n",
+                    app_manager_printf("##On byte arrive: receive end, "
+                                       "payload_size is %d.\n",
                                        ctx->message.payload_size);
                 return 1;
             }
@@ -195,7 +206,9 @@ int aee_host_msg_callback(void *msg, uint16_t msg_len)
 {
     unsigned char *p = msg, *p_end = p + msg_len;
 
-    /*app_manager_printf("App Manager receive %d bytes from Host\n", msg_len);*/
+    if (enable_log)
+        app_manager_printf("App Manager receive %d bytes from Host\n",
+                           msg_len);
 
     for (; p < p_end; p++) {
         int ret = on_imrt_link_byte_arrive(*p, &recv_ctx);
@@ -259,8 +272,8 @@ int app_manager_host_send_msg(int msg_type, const char *buf, int size)
         bh_memcpy_s(header, 2, leadings, 2);
 
         /* message type */
-        // todo: check if use network byte order!!!
-        *((uint16*) (header + 2)) = htons(msg_type);
+        /* TODO: check if use network byte order!!! */
+        *((uint16*)(header + 2)) = htons(msg_type);
 
         /* payload length */
         if (is_little_endian())
@@ -279,7 +292,8 @@ int app_manager_host_send_msg(int msg_type, const char *buf, int size)
 
         app_manager_printf("sent %d bytes to host\n", n);
         return n;
-    } else {
+    }
+    else {
         app_manager_printf("no send api provided\n");
     }
     return 0;
