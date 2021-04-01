d3cee89204ba3924d1f3e9cce4ee43aefb394715
Xu <693788454@qq.com>
2021-04-01 11:56:48 +0800

    fix aux stack covered issue

--- core/iwasm/libraries/thread-mgr/thread_manager.c
+++ core/iwasm/libraries/thread-mgr/thread_manager.c
@@ -281,6 +281,40 @@ wasm_cluster_del_exec_env(WASMCluster *cluster, WASMExecEnv *exec_env)
     return ret;
 }
 
+static WASMExecEnv *
+wasm_cluster_search_exec_env(WASMCluster *cluster,
+                             WASMModuleInstanceCommon *module_inst)
+{
+    WASMExecEnv *node = bh_list_first_elem(&cluster->exec_env_list);
+
+    while (node) {
+        if (node->module_inst == module_inst) {
+            return node;
+        }
+        node = bh_list_elem_next(node);
+    }
+
+    return NULL;
+}
+
+/* search the global cluster list to find if the given
+    module instance have a corresponding exec_env */
+WASMExecEnv *
+wasm_clusters_search_exec_env(WASMModuleInstanceCommon *module_inst)
+{
+    WASMCluster *cluster = bh_list_first_elem(cluster_list);
+    WASMExecEnv *exec_env = NULL;
+
+    while (cluster) {
+        exec_env = wasm_cluster_search_exec_env(cluster, module_inst);
+        if (exec_env)
+            return exec_env;
+        cluster = bh_list_elem_next(cluster);
+    }
+
+    return NULL;
+}
+
 WASMExecEnv *
 wasm_cluster_spawn_exec_env(WASMExecEnv *exec_env)
 {
