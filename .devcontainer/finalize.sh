76908af491aa03ddc83207d3a4fa10736455fd19
liang.he@intel.com <liang.he@intel.com>
2023-01-09 02:47:59 +0000

    Localize dev. env.
    - add my extensions
    - use privileged mode for development
    - copilot
    - intellisense -> clangd
    - jupyter notebook
    - SGX
    - opam

---
+++ .devcontainer/finalize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -euvx
+
+printf "Running 'postCreateCommand' Script ...\n"
+
+printf "Installing Rust Targets\n"
+sudo chown -R vscode ${CARGO_HOME}
+sudo chown -R vscode ${RUSTUP_HOME}
+# rustup update stable --no-self-update
+# rustup default stable
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-wasi
+rustup component add clippy
+rustup component add rustfmt
+
+printf "Installing Python Dependencies\n"
+pip install -r .devcontainer/requirements.txt
+
+printf "Installing NPM Dependencies\n"
+
+printf "Installing Ocaml stuff\n"
+opam init --yes --shell-setup
+eval $(opam env --switch=default)
+opam install --yes dune menhir
+
+printf "'postCreateCommand' Script Done\n"
