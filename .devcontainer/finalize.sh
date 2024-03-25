bce34b3c8fb0b8bc011263e2c837b51715df5cf8
liang.he@intel.com <liang.he@intel.com>
2024-03-25 23:08:51 +0000

    optimize devcontainer for codespace

---
+++ .devcontainer/finalize.sh
@@ -0,0 +1,10 @@
+printf "Running 'postCreateCommand' Script\n"
+
+# Install Rust Targets
+printf "Installing Rust Targets\n"
+
+# Install Python stuff
+printf "Installing Python Dependencies"
+
+# Install NPM dependencies
+printf "Installing NPM Dependencies"
