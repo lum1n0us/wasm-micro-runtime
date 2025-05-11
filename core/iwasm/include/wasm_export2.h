1a478a5cd4f0561f37b6bfee7c267cd3f59ad359
liang.he@intel.com <liang.he@intel.com>
2025-05-12 07:39:49 +0800

    Implement a thread-level-singleton WASMRuntime
    Plan to replace process-level global variables with thread-local variables.
    The reason for not using the variable attribute `__thread` is due to
    concerns that it is a GCC extension and requires significant support from the
    linker (ld), dynamic linker (ld.so), and system libraries
    (libc.so and libpthread.so), making it not available everywhere.
    
    Plus, memory initialization

---
+++ core/iwasm/include/wasm_export2.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#ifndef _WASM_EXPORT_2_H
+#define _WASM_EXPORT_2_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WASM_EXPORT_2_H */
