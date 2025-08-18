bf3f611102b5689ece293f9f9bfccf2b1208423f
lum1n0us <liang.he@intel.com>
2025-08-18 05:55:28 +0000

    remove rust and emsdk and other components unnecessary

--- .devcontainer/finalize.sh
+++ .devcontainer/finalize.sh
@@ -2,15 +2,15 @@
 
 printf "Running 'postCreateCommand' Script ...\n"
 
-printf "Installing Rust Targets\n"
-sudo chown -R vscode ${CARGO_HOME}
-sudo chown -R vscode ${RUSTUP_HOME}
-# rustup update stable --no-self-update
-# rustup default stable
-#rustup target add wasm32-unknown-unknown
-RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup target add wasm32-wasip1
-#rustup component add clippy
-RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup component add rustfmt
+# printf "Installing Rust Targets\n"
+# sudo chown -R vscode ${CARGO_HOME}
+# sudo chown -R vscode ${RUSTUP_HOME}
+# # rustup update stable --no-self-update
+# # rustup default stable
+# #rustup target add wasm32-unknown-unknown
+# RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup target add wasm32-wasip1
+# #rustup component add clippy
+# RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup component add rustfmt
 
 printf "Installing Python Dependencies\n"
 pip install -r .devcontainer/requirements.txt
