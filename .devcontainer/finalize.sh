80ce1e9a5fd43b7cf6993321c633e1cf5778d4e8
liang.he@intel.com <liang.he@intel.com>
2025-09-04 09:41:54 +0000

    [removeme] maintain a lite version

--- .devcontainer/finalize.sh
+++ .devcontainer/finalize.sh
@@ -2,20 +2,21 @@
 
 printf "Running 'postCreateCommand' Script ...\n"
 
-printf "Installing Rust Targets\n"
-sudo chown -R vscode ${CARGO_HOME}
-sudo chown -R vscode ${RUSTUP_HOME}
-# rustup update stable --no-self-update
-# rustup default stable
-#rustup target add wasm32-unknown-unknown
-RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup target add wasm32-wasip1
-#rustup component add clippy
-RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup component add rustfmt
+# printf "Installing Rust Targets\n"
+# sudo chown -R vscode ${CARGO_HOME}
+# sudo chown -R vscode ${RUSTUP_HOME}
+# # rustup update stable --no-self-update
+# # rustup default stable
+# #rustup target add wasm32-unknown-unknown
+# RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup target add wasm32-wasip1
+# #rustup component add clippy
+# RUSTUP_DIST_SERVER=https://fastly-static.rust-lang.org rustup component add rustfmt
 
 printf "Installing Python Dependencies\n"
-pip install -r .devcontainer/requirements.txt
+pip install --break-system-packages --user --upgrade pip
+pip install --break-system-packages --user -r .devcontainer/requirements.txt
 
-printf "Installing NPM Dependencies\n"
+#printf "Installing NPM Dependencies\n"
 
 printf "Installing Ocaml stuff\n"
 opam init --yes --shell-setup
