e236831930e775b1e2d2aadd04669c2c5e101486
liang.he@intel.com <liang.he@intel.com>
2026-02-01 13:44:27 +0800

    install opencode and tools for it

--- .devcontainer/local/finalize.sh
+++ .devcontainer/local/finalize.sh
@@ -1,3 +1,7 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+
 echo "Running local finalize script..."
 
 #
@@ -5,5 +9,8 @@ echo "Running local finalize script..."
 #
 # Install required packages
 pip3 install --no-cache-dir --break-system-packages -r .devcontainer/requirements.txt
+
+# Install opencode
+curl -fsSL https://opencode.ai/install | bash
 
 echo "Local finalize script completed. ✅"
