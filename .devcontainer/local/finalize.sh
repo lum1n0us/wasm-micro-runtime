5aff28cf5efbc79b0bb2ef49be75c06c26bd7645
liang.he@intel.com <liang.he@intel.com>
2026-02-04 13:23:23 +0800

    create a directory to store auth information and tip users

--- .devcontainer/local/finalize.sh
+++ .devcontainer/local/finalize.sh
@@ -13,4 +13,7 @@ pip3 install --no-cache-dir --break-system-packages -r .devcontainer/requirement
 # Install opencode
 curl -fsSL https://opencode.ai/install | bash
 
+echo "You might want to copy your Opencode auth.json to ~/.local/share/opencode/auth.json"
+mkdir -p ~/.local/share/opencode
+
 echo "Local finalize script completed. ✅"
